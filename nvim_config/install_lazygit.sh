ad497adb2fc2057522b7e395f1cfce195d369f52
konnor <konnor@human.human>
2024-05-23 14:47:12 +0500

    fix lazygit install path

--- nvim_config/install_lazygit.sh
+++ nvim_config/install_lazygit.sh
@@ -5,7 +5,7 @@ source ./VARS.sh
 URL=https://github.com/jesseduffield/lazygit/releases/tag/v0.42.0/lazygit_0.42.0_Linux_x86_64.tar.gz
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+curl -Lo $SRC_DIR/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm -f ./lazygit
