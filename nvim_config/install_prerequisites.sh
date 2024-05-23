353eb9114585b64f38c652552d8468dd1e4cd110
konnor <konnor@human.human>
2024-05-23 14:38:02 +0500

    add '-y' flag to deps installation

--- nvim_config/install_prerequisites.sh
+++ nvim_config/install_prerequisites.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-sudo apt install ninja-build gettext cmake unzip curl build-essential
+sudo apt install ninja-build gettext cmake unzip curl build-essential -y
 
 # TODO: find way for autoinstall py-env and pip
 
 # this need for lsp modules
 # also need install python env and pip for current python version
-sudo apt install npm cargo
+sudo apt install npm cargo -y
