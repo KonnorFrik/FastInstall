646d4f0a6c7bed1df17e00aebbd9dad9ed5feef8
konnor <konnor@human.human>
2024-05-23 14:52:45 +0500

    fix 2.0 lazygit install path

--- nvim_config/VARS.sh
+++ nvim_config/VARS.sh
@@ -1,3 +1,4 @@
 NVIM_CONFIG_PATH=$HOME/.config
 SAVE_DIR=./nvim
 SRC_DIR=$HOME/sources
+LAZY_GIT_SRC_DIR=$SRC_DIR/lazygit
--- nvim_config/install_lazygit.sh
+++ nvim_config/install_lazygit.sh
@@ -2,10 +2,8 @@
 
 source ./VARS.sh
 
-URL=https://github.com/jesseduffield/lazygit/releases/tag/v0.42.0/lazygit_0.42.0_Linux_x86_64.tar.gz
-
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo $SRC_DIR/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-rm -f ./lazygit
+curl -Lo $LAZY_GIT_SRC_DIR/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf $LAZY_GIT_SRC_DIR/lazygit.tar.gz $LAZY_GIT_SRC_DIR/lazygit
+sudo install $LAZY_GIT_SRC_DIR/lazygit /usr/local/bin
+rm -f $LAZY_GIT_SRC_DIR/lazygit
