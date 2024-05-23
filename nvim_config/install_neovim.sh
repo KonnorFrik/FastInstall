96a0c724e5e4193288a2ab347efeb1577b50a9ab
konnor <konnor@human.human>
2024-05-23 14:32:47 +0500

    fix installation bug

--- nvim_config/install_neovim.sh
+++ nvim_config/install_neovim.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 source ./VARS.sh
+NVIM_SRC_DIR=$SRC_DIR/neovim
 
-git clone https://github.com/neovim/neovim $SRC_DIR
-cd $SRC_DIR/neovim 
+git clone https://github.com/neovim/neovim $NVIM_SRC_DIR
+cd $NVIM_SRC_DIR 
 git checkout stable
 make CMAKE_BUILD_TYPE=Release
 sudo make install
