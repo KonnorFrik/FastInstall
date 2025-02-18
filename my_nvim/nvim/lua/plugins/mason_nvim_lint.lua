661a683ec48b94ec62795c976040f3308bcd33a2
Konnor <konnor.frik666@gmail.com>
2025-02-18 18:54:34 +0500

    add mason nvim lint; tried to setup it

---
+++ my_nvim/nvim/lua/plugins/mason_nvim_lint.lua
@@ -0,0 +1,8 @@
+return {
+    "williamboman/mason.nvim",
+    "mfussenegger/nvim-lint",
+    "rshkarin/mason-nvim-lint",
+    -- idk is next 2 lines neccessary
+    -- priority = 1000,
+    dependencies = { "mfussenegger/nvim-lint" },
+}
